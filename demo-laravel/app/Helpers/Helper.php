502175558073ee3cdd11b1a17ba7eb8a2120e08d
Minh Trung <nguyenducminhtrung0044@gmail.com>
2022-02-22 22:20:09 +0700

    Load Prpoduct

--- demo-laravel/app/Helpers/Helper.php
+++ demo-laravel/app/Helpers/Helper.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Helpers;
+use Illuminate\Support\Str;
 
 class Helper {
 
@@ -34,25 +35,36 @@ class Helper {
     }
     public static function menus($menus , $parent_id=0){
         $html='';
-        foreach ($menus as $key=>$menu){
+        foreach ($menus as $key => $menu) {
             if ($menu->parent_id == $parent_id) {
                 $html .= '
-                <tr>
-                <td>'.$menu->id.'</td>
-                <td>'.$char.$menu->name.'</td>
-                <td>'.self::active($menu->active).'</td>
-                <td>'.$menu->updated_at.'</td>
-                <td>
-                    <a  class="btn btn-primary btn-sm" href="/admin/menus/edit/'.$menu->id.'"><i class="fas fa-edit"></i></a>
-                    <a  class="btn btn-danger btn-sm" onclick="removeRow(' . $menu->id . ', \'/admin/menus/destroy\')")" href="#"><i class="fas fa-trash"></i></a>
-                </td>
-              </tr>';
-              unset($menus[$key]);
-              $html .= self::menu($menus, $menu->id, $char . '|--');
-            }        
+                    <li>
+                        <a href="/danh-muc/' . $menu->id . '-' . Str::slug($menu->name, '-') . '.html">
+                            ' . $menu->name . '
+                        </a>';
 
+                unset($menus[$key]);
+
+                if (self::isChild($menus, $menu->id)) {
+                    $html .= '<ul class="sub-menu">';
+                    $html .= self::menus($menus, $menu->id);
+                    $html .= '</ul>';
+                }
+
+                $html .= '</li>';
+            }
         }
         return $html;
+    }
+    public static function isChild($menus, $id) : bool
+    {
+        foreach ($menus as $menu) {
+            if ($menu->parent_id == $id) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- demo-laravel/app/Http/Controllers/Admin/MainController.php
+++ demo-laravel/app/Http/Controllers/Admin/MainController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Services\Slider\SliderService;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+
+    protected $sliderService;
+
+    public function _constructor(SliderService $slider){
+        $this->sliderService = $slider;
+    }
     public function index() {
-        return view('admin.main',['title'=> 'Trang Quản Trị Hệ Thống']);
+        return view('admin.main',[
+            'title'=> 'Trang Quản Trị Hệ Thống',
+
+    ]);
     }
     public function welcome() {
         return view('welcome');
